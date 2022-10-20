8e2f91e4003f6c8b2762357104037aa4fa0c7819
nauticala <randifisher2019@gmail.com>
2022-10-20 01:42:39 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 function getComputerChoice() {
-    const computerChoice = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * computerChoice.length);
+    const computerSelection = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * computerSelection.length);
 
-    return random, computerChoice[random];
+    return random, computerSelection[random];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    let computerScore = 0;
+    let playerScore = 0;
+
+    if (computerSelection == playerSelection) {
+        return "It's a tie!";
+    }
+    else if (computerSelection == "rock" && playerSelection == "scissors") {
+    computerScore =+ computerScore;
+    return "You lose! Rock crushes scissors!";
+    }
+    else if (computerSelection == "scissors" && playerSelection == "paper") {
+        computerScore =+ computerScore;
+        return "You lose! Scissors cut paper!";
+    }
+    else if (computerSelection == "paper" && playerSelection == "rock") {
+        computerScore =+ computerScore;
+        return "You lose! Paper covers rock! Or something.";
+    }
+    else {
+        playerScore =+ playerScore
+        return "YOU WIN! LOOK AT YOU GO!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
