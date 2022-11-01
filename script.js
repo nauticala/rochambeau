895e02fcb36cbc31ba117e541862a9a53ed8cc11
nauticala <randifisher2019@gmail.com>
2022-11-01 13:47:49 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("Rock, paper, scissors, shoot!")
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
